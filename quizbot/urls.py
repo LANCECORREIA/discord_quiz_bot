5f6fff380bd5a6bb09946090661abf3892c7e9f3
Deadpool <abcd162003@gmail.com>
2022-06-14 01:18:28 +0530

    added leaderboard api

--- quizbot/urls.py
+++ quizbot/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from quiz.views import RandomQuestion
-from score.views import UpdateScores
+from score.views import UpdateScores, Leaderboard
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/random/", RandomQuestion.as_view(), name="random"),
     path("api/score/update", UpdateScores.as_view(), name="score_update"),
+    path("api/score/leaderboard", Leaderboard.as_view(), name="score"),
 ]
--- score/views.py
+++ score/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .serializers import ScoreSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework import status
 from .models import Score
 
@@ -21,3 +22,11 @@ class UpdateScores(APIView):
             serializer.save()
             return Response(None, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Leaderboard(ListAPIView):
+    serializer_class = ScoreSerializer
+    queryset = Score.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.order_by("-points")[:10]
