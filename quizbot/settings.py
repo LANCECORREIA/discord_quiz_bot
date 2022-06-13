16e26514f738e68da2649807414a7a6724987c97
Deadpool <abcd162003@gmail.com>
2022-06-13 18:51:25 +0530

    commit check

--- quizbot/settings.py
+++ quizbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.8000"]
 
 
 # Application definition
